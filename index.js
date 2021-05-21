a8f328f9a6f4dce8eb8f74f1e5a35b016481aa93
Praxis Technologies <ptanalytics@gmail.com>
2021-05-21 18:18:32 -0400

    answered questions for test

--- index.js
+++ index.js
@@ -15,9 +15,16 @@ The function should:
   Example createMenuItem('tacos', 8, 'Lunch') should return {name: 'tacos', price: 8, category: 'Lunch'}
 */
 
-function createMenuItem(/*Your code here*/){
-    /*Your code here*/
+function createMenuItem(name, price, category) {
+  return {
+    name: name,
+    price: price, 
+    category: category
+  }
 }
+
+createMenuItem('tacos', 8, 'Lunch')
+
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 1b: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Invoke your function!
@@ -29,6 +36,12 @@ Test your createMenuItems function by doing the following:
   For example: createMenuItem("pizza",5,"lunch") would return this as the object: {name:"Pizza",price:5,category:"lunch"}
 */
 
+
+createMenuItem('pizza', 5, 'Lunch')
+
+createMenuItem('bean burrito', 8, 'Lunch')
+
+createMenuItem('Soda', 2, 'Lunch')
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
@@ -44,12 +57,28 @@ Using the burger object below do the following:
   For example: burger.discount("teacher") would return 13.5 and burger.discount("public") would return 16.2
 */
 
+
 export const burger = {
   name: "Burger", 
   price: 18, 
   category: "Lunch", 
-  /*Your code here*/
+  discount: function(customer){
+    if(customer === "teacher"){
+      return this.price - (this.price * 0.25)
+    }
+    else if (customer === "student"){
+      return this.price - (this.price * 0.25)
+    }
+    else if(customer === "public"){
+      return this.price - (this.price * 0.1)
+    }
+  }
 }
+
+console.log(burger.discount("teacher"))
+console.log(burger.discount("student"))
+console.log(burger.discount("public"));
+
 
 
 
@@ -70,7 +99,7 @@ Using the reviews array above:
   1. log only Julius' feedback to the console - no function needed 
 */
 
-
+console.log(reviews[5].feedback)
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
@@ -78,6 +107,9 @@ Using the reviews array above do the following: (no function needed)
   1. Following the same format (name, rating, feedback), add a new fictitious review object to the reviews array
   2. log the whole array to the console, make sure the new review is inside of it   
 */
+
+//reviews.unshift({name: "Jack", rating: 5, feedback: "THE BEST OF THE BEST."});
+//console.log(reviews)
 
 
 
@@ -87,6 +119,8 @@ Reyna's feedback is missing! Use what you know to do the following: (no function
   2. log the reviews array to the console to check your work
 */
 
+reviews[7].feedback = "this place is chill with really cool people, great for getting work done on weekdays";
+console.log(reviews)
 
 
 
@@ -102,9 +136,11 @@ Use the getReviewByIndex function below to do the following:
 */
 
 
-function getReviewByIndex(/*Your code here*/) {
-  /*Your code here*/
+function getReviewByIndex(reviews, index) {
+  return `${reviews[index].name} gave the restaurant a ${reviews[index].rating} star review, and their feedback was: ${reviews[index].feedback}`;
 }
+
+getReviewByIndex(reviews,0)
 
 
   
@@ -114,16 +150,18 @@ Write a function to get information about the most recent (last) review called `
 
 Use the getLastReview function below to do the following:
   1. Receive an array of objects as a parameter
-  2. Return the last index as a string in the format: "{name} gave the restaurant a {rating} star review, and their feedback was: {feedback}"
+  2. Return the last index as a string in the format: "{name} gave the restaurant a {rating} star review, and theirb feedback was: {feedback}"
   3. Invoke the function with the reviews array as the argument
   
   For example: getLastReview(reviews) would return: "Reyna gave the restaurant a 3.5 star review, and their feedback was: this place is chill with really cool people, great for getting work done on weekdays".
 */
 
 
-function getLastReview(/*Your code here*/) {
-  /*Your code here*/
+function getLastReview(reviews) {
+  return `${reviews[reviews.length - 1].name} gave the restaurant a ${reviews[reviews.length - 1].rating} star review, and their feedback was: ${reviews[reviews.length - 1].feedback}`
 } 
+
+getLastReview(reviews)
 
 
 
